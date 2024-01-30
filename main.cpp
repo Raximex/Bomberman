eee1f2c209339a3760be4ec0c35160a95703dcc6
Ata <atatoz60@gmail.com>
2024-01-30 15:09:47 +0100

    Déplacement du joueur

--- main.cpp
+++ main.cpp
@@ -1,24 +1,50 @@
 //#include "src/Graphics/Graphics.h"
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
-{
-    sf::Window(sf::VideoMode(800, 600), "Bomberman");
-    sf::RenderWindow window(sf::VideoMode(200,200), "Hello From SFML");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Magenta);
+{   sf::ContextSettings settings;
+    settings.antialiasingLevel=8;
+    sf::RenderWindow window(sf::VideoMode(800,600),"BOMBERMAN", sf::Style::Default, settings);
+    sf::Texture textureJoueur;
+    sf::Sprite  spriteJoueur;
+
+    if(textureJoueur.loadFromFile("assets/SpriteBomberman1.png"))
+    {
+        std::cout << "Image chargée" << std::endl;
+        spriteJoueur.setTexture(textureJoueur);
+        spriteJoueur.setTextureRect(sf::IntRect (55,0,18,28));
+    }
+    else{
+        std::cout << "Image non chargée" << std::endl;
+    }
 
     while (window.isOpen())
     {
         sf::Event event;
+
         while (window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed){
+        {       if(event.type == sf::Event::Closed){
                 window.close();
             }
+
         }
+
+        sf::Vector2f  position = spriteJoueur.getPosition();
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            spriteJoueur.setPosition(position+sf::Vector2f (1,0));
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+            spriteJoueur.setPosition(position+sf::Vector2f (0,-1));
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            spriteJoueur.setPosition(position+sf::Vector2f (0,1));
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+            spriteJoueur.setPosition(position+sf::Vector2f (-1,0));
+
         window.clear();
-        window.draw(shape);
+        window.draw(spriteJoueur);
         window.display();
     }
 
